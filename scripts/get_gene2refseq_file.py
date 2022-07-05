e588b08ebad8eb13a560da426d0ed6b39b1e1652
dkoslicki <dmkoslicki@gmail.com>
2022-07-05 15:40:38 -0400

    add default user name and password, PEP8 fixes #9

--- scripts/get_gene2refseq_file.py
+++ scripts/get_gene2refseq_file.py
@@ -9,24 +9,28 @@ def main():
                                     " then will download the gene2refseqfile, to procceed you need to"
                                     " modify the username and password of the ftp login (line 16)."
                                     ,formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.parse_args()
+    parser.add_argument('-u', '--user_name', type=str, help="User name for the ftp login. NCBI wants an email address.",
+                        default="user@email.com")
+    args = parser.parse_args()
+    user_name = args.user_name
     # login to ftp and direct to needed directory
     print("Connecting...")
     try:
         ftp = FTP('ftp.ncbi.nlm.nih.gov')
-        ftp.login(user = "#####", passwd = "#####") # login creds to the ftp server
+        ftp.login(user="anonymous", passwd=f"{user_name}")  # login creds to the ftp server
         ftp.cwd('gene/DATA/') 
         ftp.encoding = "utf-8"
         filename = "gene2refseq.gz"
     except:
-        print("ALERT: Connection Failed! (check network or ftp credentials")
+        raise Exception("ALERT: Connection Failed! (check network or ftp credentials")
     print("downloading...")
     try:
-        ftp.retrbinary("RETR " + filename, open(filename, 'wb').write) # download needed file
+        ftp.retrbinary("RETR " + filename, open(filename, 'wb').write)  # download needed file
     except:
-        print("ALERT: Downloading Failed! (check disk space or connection)")
-    ftp.quit() # quit connection
+        raise Exception("ALERT: Download Failed! (check disk space or connection)")
+    ftp.quit()  # quit connection
     print("Done!")
+
 
 if __name__ == "__main__":
     main()
