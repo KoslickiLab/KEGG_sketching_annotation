b23618f00aaa5e19389b08f8c20bd9da1497c3a4
Mahmudur <mahmudhera93@gmail.com>
2022-12-02 14:06:59 -0500

    added snakemake

---
+++ experiments/impact_of_num_reads_on_kegg_data/setup.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -e
+set -u
+scriptDir="../../scripts"
+dataDir="data"
+mkdir -p $dataDir
+
+# File extensions must be fa/fasta/faa otherwise bbmap doesn't know how to parse them
+genomeDatabaseFull="$dataDir/genome_ref_full.fa"
+genomeDatabaseTruncated="$dataDir/genome_ref_truncated.fa"
+proteinDatabaseFull="$dataDir/protein_ref_full.faa"
+proteinDatabaseTruncated="$dataDir/protein_ref_truncated.faa"
+simulatedMetagenome="$dataDir/simulatedMetagenome.fastq"
+genomePath="../extracted_genomes_from_kegg"
+
+# set variables
+numGenomes=30
+numGenomesFullDB=$numGenomes
+numGenomesTruncatedDB=20
+#numReads=100000
+readLen=150
+numGenes=1000
+kSize=7  # decreasing this increases sensitivity at cost of FP's
+refScale=10  # the higher this number, the faster things run, the smaller the database, at the cost of less sensitivity
+queryScale=1  # likely will want to keep this at one (no down-sampling of the query)
+thresholdBP=50  # this has the largest impact on FNs and FPs: setting it higher filters out more false positives, at the cost of more false negatives
+
+# create the full genome reference database
+echo "$scriptDir/create_genome_ref_db.py $genomePath $genomeDatabaseFull $numGenomesFullDB"
+$scriptDir/create_genome_ref_db.py $genomePath $genomeDatabaseFull $numGenomesFullDB
+
+# create the truncated genome reference database
+echo "$scriptDir/create_genome_ref_db.py $genomePath $genomeDatabaseTruncated $numGenomesTruncatedDB"
+$scriptDir/create_genome_ref_db.py $genomePath $genomeDatabaseTruncated $numGenomesTruncatedDB
+
+# create the mapping files required for the protein database
+echo "$scriptDir/make_mapping_file.py "$genomePath""
+#$scriptDir/make_mapping_file.py "$genomePath"
+# no need because all genomes have mapping files ready
+
+# create the protein reference database
+echo "$scriptDir/create_gene_ref_db.py "$genomePath" $proteinDatabaseFull $numGenomesFullDB"
+$scriptDir/create_gene_ref_db.py "$genomePath" $proteinDatabaseFull $numGenomesFullDB
+echo "$scriptDir/create_gene_ref_db.py "$genomePath" $proteinDatabaseTruncated $numGenomesTruncatedDB"
+$scriptDir/create_gene_ref_db.py "$genomePath" $proteinDatabaseTruncated $numGenomesTruncatedDB
